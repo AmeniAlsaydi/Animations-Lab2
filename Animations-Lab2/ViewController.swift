c8fef29cbd4b825437bfde1f631b5fc8447f870a
Ameni Alsaydi <amenialsaydi@pursuit.org>
2020-02-05 10:10:59 -0500

    Initial Commit

---
+++ Animations-Lab2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Animations-Lab2
+//
+//  Created by Amy Alsaydi on 2/5/20.
+//  Copyright © 2020 Amy Alsaydi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
